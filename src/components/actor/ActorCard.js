331e415bddebe44911a30c4ea46657b0777f9fe1
tonirul <tonirulislam@outlook.com>
2022-02-07 11:28:37 +0530

    added show page

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -10,12 +10,14 @@ const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
       </div>
       <h1>
         {name} 
+        {' '}
         {gender ? `(${gender})` : null}
       </h1>
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
       {birthday ? (
         <p>
           Born
+          {' '}
           {birthday}
         </p>
 ) : null}
